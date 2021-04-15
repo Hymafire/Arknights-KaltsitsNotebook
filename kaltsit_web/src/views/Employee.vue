b187f5b262d21fb7876d5d5926305484d25e4095
Hymafire <hu464997669@qq.com>
2021-04-15 19:55:52 +0800

    commit v0.2.0

--- kaltsit_web/src/views/Employee.vue
+++ kaltsit_web/src/views/Employee.vue
@@ -2,10 +2,10 @@
   <el-container class="home-container">
     <!--  左  -->
     <el-aside width="220px" class="home-aside">
-      <el-input prefix-icon="el-icon-user" placeholder="输入干员名称" v-model="filterText" class="search-input">
-        <el-button slot="append" icon="el-icon-search"></el-button>
+      <el-input prefix-icon="el-icon-user" placeholder="输入干员名称" v-model="searchname" class="search-input">
+        <el-button slot="append" icon="el-icon-search" @click="findEmployee"/>
       </el-input>
-      <el-tree :data="employeeList" :props="props" accordion @node-click="handleNodeClick" highlight-current> </el-tree>
+      <el-tree :data="employeeList" :props="props" accordion @node-click="handleNodeClick" highlight-current />
     </el-aside>
     <!--  主体  -->
     <el-main>
@@ -25,19 +25,20 @@
         </div>
         <!-- 第一排数据 -->
         <el-row :gutter="20" type="flex">
-          <el-col :span="16" :offset="7">
-            <el-table :data="em_param">
-              <el-table-column label="属性："></el-table-column>
+          <el-col :span="2" :offset="7" />
+          <el-col :span="13">
+            <el-table :data="[employee]">
+              <el-table-column label="属性列表：" prop="itemUsage" />
             </el-table>
           </el-col>
         </el-row>
         <el-row :gutter="20" type="flex">
           <el-col :span="16" :offset="7">
             <el-table :data="[em_param]">
-              <el-table-column label="最大生命" prop="maxHp"></el-table-column>
-              <el-table-column label="攻击" prop="atk"></el-table-column>
-              <el-table-column label="防御" prop="def"></el-table-column>
-              <el-table-column label="法抗" prop="magicResistance"></el-table-column>
+              <el-table-column label="最大生命" prop="maxHp" />
+              <el-table-column label="攻击" prop="atk" />
+              <el-table-column label="防御" prop="def" />
+              <el-table-column label="法抗" prop="magicResistance" />
             </el-table>
           </el-col>
         </el-row>
@@ -45,22 +46,46 @@
         <el-row :gutter="20" type="flex">
           <el-col :span="16" :offset="7">
             <el-table :data="[em_param]">
-              <el-table-column label="部署费用" prop="cost"></el-table-column>
-              <el-table-column label="阻挡数" prop="blockCnt"></el-table-column>
-              <el-table-column label="攻击间隔" prop="baseAttackTime"></el-table-column>
-              <el-table-column label="再部署时间" prop="respawnTime"></el-table-column>
+              <el-table-column label="部署费用" prop="cost" />
+              <el-table-column label="阻挡数" prop="blockCnt" />
+              <el-table-column label="攻击间隔" prop="baseAttackTime" />
+              <el-table-column label="再部署时间" prop="respawnTime" />
             </el-table>
           </el-col>
         </el-row>
         <!-- 第三排数据 -->
         <el-row :gutter="20" type="flex">
           <el-col :span="16" :offset="7">
-            <el-table :data="[employee]">
-              <el-table-column label="天赋" prop="talents"></el-table-column>
+            <el-table :data="[em_talent]">
+              <el-table-column label="天赋" prop="talents" />
             </el-table>
           </el-col>
         </el-row>
-        <!-- 分页 -->
+        <!-- 数据输入栏 -->
+        <el-form
+          ref="loginFormRef"
+          :model="infoForm"
+          :rules="infoRules"
+          label-width="0px"
+          class="info_form"
+        >
+          <el-form-item prop="elite">
+            <span>精英化等级：</span>
+            <el-input prefix-icon="el-icon-user" v-model="infoForm.elite" @keyup.enter.native="getEmployeeParam"/>
+          </el-form-item>
+          <el-form-item prop="level">
+            <span>干员等级：</span>
+            <el-input prefix-icon="el-icon-user" v-model="infoForm.level" @keyup.enter.native="getEmployeeParam"/>
+          </el-form-item>
+          <el-form-item prop="favor">
+            <span>信赖度：</span>
+            <el-input prefix-icon="el-icon-user" v-model="infoForm.favor" @keyup.enter.native="getEmployeeParam"/>
+          </el-form-item>
+          <el-form-item prop="potential">
+            <span>潜能：</span>
+            <el-input prefix-icon="el-icon-user" v-model="infoForm.potential" @keyup.enter.native="getEmployeeParam"/>
+          </el-form-item>
+        </el-form>
       </el-card>
     </el-main>
   </el-container>
@@ -73,30 +98,40 @@ export default {
     return {
       employeeTable: [],
       employeeList: [],
-      employeeList1: [],
-      employeeList2: [],
-      employeeList3: [],
-      employeeList4: [],
-      employeeList5: [],
-      employeeList6: [],
       employee: [],
       em_param: [],
-      total: 0,
+      em_talent: [],
       props: {
         name: 'name',
         label: 'name'
       },
       searchname: '斯卡蒂',
-      elite: '0',
-      level: '40',
-      favor: '100'
+      infoForm: {
+        elite: '0',
+        level: '1',
+        favor: '0',
+        potential: '0'
+      },
+      infoRules: {
+        elit: [
+          { required: true, message: '精英化等级', trigger: 'blur' }
+        ],
+        level: [
+          { required: true, message: '干员等级', trigger: 'blur' }
+        ],
+        favor: [
+          { required: true, message: '信赖度', trigger: 'blur' }
+        ],
+        potation: [
+          { required: true, message: '潜能', trigger: 'blur' }
+        ]
+      }
     }
   },
   // 创建时调用
   created () {
     this.getEmployeeTable()
     this.getEmployeeList()
-    this.haveEmployeeList()
   },
   mounted () {
     this.findEmployee()
@@ -106,64 +141,50 @@ export default {
     getEmployeeTable () {
       const employeeTable = require('../assets/data/employee_table.json')
       this.employeeTable = employeeTable
-      this.total = employeeTable.length
     },
     // 干员名单
     getEmployeeList () {
       var cnt = [0, 0, 0, 0, 0, 0]
+      var employeeChiList = [[], [], [], [], [], []]
+      var rarity = 0
+      // 遍历获得子列表
       for (var en in this.employeeTable) {
         if (this.employeeTable[en].profession !== 'TRAP' && this.employeeTable[en].profession !== 'TOKEN') {
-          if (this.employeeTable[en].rarity === 0) {
-            this.employeeList1[cnt[0]] = { name: this.employeeTable[en].name }
-            cnt[0]++
-          } else if (this.employeeTable[en].rarity === 1) {
-            this.employeeList2[cnt[1]] = { name: this.employeeTable[en].name }
-            cnt[1]++
-          } else if (this.employeeTable[en].rarity === 2) {
-            this.employeeList3[cnt[2]] = { name: this.employeeTable[en].name }
-            cnt[2]++
-          } else if (this.employeeTable[en].rarity === 3) {
-            this.employeeList4[cnt[3]] = { name: this.employeeTable[en].name }
-            cnt[3]++
-          } else if (this.employeeTable[en].rarity === 4) {
-            this.employeeList5[cnt[4]] = { name: this.employeeTable[en].name }
-            cnt[4]++
-          } else if (this.employeeTable[en].rarity === 5) {
-            this.employeeList6[cnt[5]] = { name: this.employeeTable[en].name }
-            cnt[5]++
-          }
+          rarity = this.employeeTable[en].rarity
+          employeeChiList[rarity][cnt[rarity]] = { name: this.employeeTable[en].name }
+          cnt[rarity]++
         }
       }
-    },
-    haveEmployeeList () {
+      // 生成列表
       this.employeeList =
       [
         {
           name: '★★★★★★',
-          children: this.employeeList6
+          children: employeeChiList[5]
         },
         {
           name: '★★★★★',
-          children: this.employeeList5
+          children: employeeChiList[4]
         },
         {
           name: '★★★★',
-          children: this.employeeList4
+          children: employeeChiList[3]
         },
         {
           name: '★★★',
-          children: this.employeeList3
+          children: employeeChiList[2]
         },
         {
           name: '★★',
-          children: this.employeeList2
+          children: employeeChiList[1]
         },
         {
           name: '★',
-          children: this.employeeList1
+          children: employeeChiList[0]
         }
       ]
     },
+    // 查找干员
     findEmployee () {
       for (var en in this.employeeTable) {
         if (this.employeeTable[en].name === this.searchname) {
@@ -171,6 +192,7 @@ export default {
           break
         }
       }
+      this.getEmployeeParam()
     },
     /* ========================================== 分界线 ========================================= */
     // 用于计算干员的基础属性
@@ -182,29 +204,35 @@ export default {
     },
     // 用于计算干员的等级数据
     employeeBaseParamClac () {
-      const min_data = this.employee.phases[this.elite].attributesKeyFrames[0].data
-      const max_data = this.employee.phases[this.elite].attributesKeyFrames[1].data
-      const diff = this.employee.phases[this.elite].maxLevel - 1
+      const min_data = this.employee.phases[this.infoForm.elite].attributesKeyFrames[0].data
+      const max_data = this.employee.phases[this.infoForm.elite].attributesKeyFrames[1].data
+      const diff = this.employee.phases[this.infoForm.elite].maxLevel - 1
       this.em_param = min_data
       // 具体参数计算
-      this.em_param.maxHp += Math.round((this.level - 1) * (max_data.maxHp - min_data.maxHp) / diff)
-      this.em_param.atk += Math.round((this.level - 1) * (max_data.atk - min_data.atk) / diff)
-      this.em_param.def += Math.round((this.level - 1) * (max_data.def - min_data.def) / diff)
+      this.em_param.maxHp += Math.round((this.infoForm.level - 1) * (max_data.maxHp - min_data.maxHp) / diff)
+      this.em_param.atk += Math.round((this.infoForm.level - 1) * (max_data.atk - min_data.atk) / diff)
+      this.em_param.def += Math.round((this.infoForm.level - 1) * (max_data.def - min_data.def) / diff)
     },
     // 用于计算信赖的加成
+    // 有bug
     employeeFavorClac () {
-      const min_favor = Math.min(this.favor, 100)
+      const min_favor = Math.min(this.infoForm.favor, 100)
       const favor_data = this.employee.favorKeyFrames[1].data
+      // 存储基础数据
       this.em_param.maxHp += Math.round(favor_data.maxHp * min_favor / 100)
       this.em_param.atk += Math.round(favor_data.atk * min_favor / 100)
       this.em_param.def += Math.round(favor_data.def * min_favor / 100)
-      console.log(this.em_param)
+    },
+    // 用于计算潜能的加成
+    employeeTalentClac () {
     },
     // 点击事件
     handleNodeClick (data) {
       this.searchname = data.name
       this.findEmployee()
-      this.getEmployeeParam()
+    },
+    resetLoginForm () {
+      this.$refs.infoRef.resetFields()
     }
   }
 }
@@ -248,5 +276,13 @@ export default {
 }
 .head-title {
   font-weight: bold;
+}
+// 输入框
+.info_form {
+  position: absolute;
+  top: 171px;
+  width: 22%;
+  padding: 20px;
+  box-sizing: border-box;
 }
 </style>
--- kaltsit_web/vue.config.js
+++ kaltsit_web/vue.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  publicPath: ''
+  publicPath: '',
+  chainWebpack: config => {
+    // ========================== 发布 =============================
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config.entry('app').clear().add('./src/main-prod.js')
+      config.set('externals', {
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        axios: 'axios',
+        echarts: 'echarts',
+        nprogress: 'NProgress'
+      })
+    })
+    // ========================== 开发 =============================
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config.entry('app').clear().add('./src/main-dev.js')
+    })
+  }
 }
